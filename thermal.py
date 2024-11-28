b04df32be6509444eb70ae7a2b02b1fd969d2ab0
Sathvik Kanna <k.sathvik2003@gmail.com>
2024-11-28 16:54:10 +0530

    Update thermal.py

--- thermal.py
+++ thermal.py
@@ -92,6 +92,7 @@ def snapshot(heatmap):
 	#cv2.imwrite("TC001"+now+".png", heatmap)
 	heatmap = cv2.rotate(heatmap, cv2.ROTATE_90_COUNTERCLOCKWISE)
 	cv2.imwrite("1.png", heatmap)
+	print("image written")
 	return snaptime
  
 
